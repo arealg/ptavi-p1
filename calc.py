d1e423fc27409cfd878921b656dba7843c4eb3f6
arealg <anavm4_@hotmail.com>
2015-09-21 16:51:56 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -11,32 +11,24 @@ def suma(x,y):
 def resta(x,y):
     return x-y
 
-
 sum1 = sys.argv[1]
 sum2 = sys.argv[3]
 operacion = sys.argv[2]
 
-operando1 = sum1.find('.')
-operando2 = sum2.find('.')
+def find_float(sumando):
+	if '.' in sumando:
+		return float(sumando)
+	else:
+		return int(sumando)
 
 try:
-    if operando1 and operando2 == 1:
-        sum1 = float(sys.argv[1])
-        sum2 = float(sys.argv[3])
-    elif operando1 == 1 and operando2 == -1:
-        sum1 = float(sys.argv[1])
-        sum2 = int(sys.argv[3])
-    elif operando1 == -1 and operando2 == 1:
-        sum1 = int(sys.argv[1])
-        sum2 = float(sys.argv[3])
-    elif operando1 and operando2 == -1:
-        sum1 = int(sys.argv[1])
-        sum2 = int(sys.argv[3])
+	numero1 = find_float(sum1)
+	numero2 = find_float(sum2)
 
-    if operacion == 'suma':
-        print(suma(sum1,sum2))
-    elif operacion == 'resta':
-        print(resta(sum1,sum2))
+	if operacion == 'suma':
+		print(suma(numero1,numero2))
+	elif operacion == 'resta':
+		print(resta(numero1,numero2))
 
 except:
     print ('Error')

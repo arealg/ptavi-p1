86379e4f86d4c0c273194565904fdcc2cdccfec7
ana real gijon <areal@zeta40.aulas.gsyc.es>
2015-09-21 16:19:09 +0200

    Primera realizacion

--- calc.py
+++ calc.py
@@ -5,15 +5,38 @@
 
 import sys
 
-def main():
-    pass
-
 def suma(x,y):
 	return x+y
 
+def resta(x,y):
+    return x-y
 
-if __name__ == "__main__":
-	sum1 = sys.argv[1]
-	sum2 = sys.argv[2]
-    print (suma(sum1,sum2))
-	
+
+sum1 = sys.argv[1]
+sum2 = sys.argv[3]
+operacion = sys.argv[2]
+
+operando1 = sum1.find('.')
+operando2 = sum2.find('.')
+
+try:
+    if operando1 and operando2 == 1:
+        sum1 = float(sys.argv[1])
+        sum2 = float(sys.argv[3])
+    elif operando1 == 1 and operando2 == -1:
+        sum1 = float(sys.argv[1])
+        sum2 = int(sys.argv[3])
+    elif operando1 == -1 and operando2 == 1:
+        sum1 = int(sys.argv[1])
+        sum2 = float(sys.argv[3])
+    elif operando1 and operando2 == -1:
+        sum1 = int(sys.argv[1])
+        sum2 = int(sys.argv[3])
+
+    if operacion == 'suma':
+        print(suma(sum1,sum2))
+    elif operacion == 'resta':
+        print(resta(sum1,sum2))
+
+except:
+    print ('Error')
